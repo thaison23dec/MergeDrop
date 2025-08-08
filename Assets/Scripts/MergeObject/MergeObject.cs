232cc34e89c535e0e818fc617884cfb5039b5f9e
Thai Son <ngthaison2312@gmail.com>
2025-08-08 15:53:17 +0700

    update type

--- Assets/Scripts/MergeObject/MergeObject.cs
+++ Assets/Scripts/MergeObject/MergeObject.cs
@@ -6,6 +6,8 @@ public class MergeObject : MonoBehaviour
 {
     public int ID;
 
+    [SerializeField] private GameObject mergedObj;
+    [SerializeField] public MergeObjectType type;
     private GameObject block1;
     private GameObject block2;
 
@@ -18,13 +20,16 @@ public class MergeObject : MonoBehaviour
     {
         if (gameObject.CompareTag(collision.gameObject.tag))
         {
-            block1 = gameObject;
-            block2 = collision.gameObject;
-            if (ID < block2.gameObject.GetComponent<MergeObject>().ID) return;
-            Vector2 mergedObjPos = new Vector2((block1.transform.position.x + block1.transform.position.x) / 2, (block1.transform.position.y + block1.transform.position.y) / 2);
-            Instantiate(Resources.Load("SquareObject"), mergedObjPos, Quaternion.identity);
-            Destroy(gameObject);
-            Destroy(collision.gameObject);
+            if(type == collision.gameObject.GetComponent<MergeObject>().type)
+            {
+                block1 = gameObject;
+                block2 = collision.gameObject;
+                if (ID < block2.gameObject.GetComponent<MergeObject>().ID) return;
+                Vector2 mergedObjPos = new Vector2((block1.transform.position.x + block1.transform.position.x) / 2, (block1.transform.position.y + block1.transform.position.y) / 2);
+                Instantiate(mergedObj, mergedObjPos, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
