2b0a704b794905ec5f1b14bbcfd8fe51e944ba11
Thai Son <ngthaison2312@gmail.com>
2025-08-08 15:06:56 +0700

    merge test commit

--- Assets/Scripts/MergeObject/MergeObject.cs
+++ Assets/Scripts/MergeObject/MergeObject.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MergeObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int ID;
+
+    private GameObject block1;
+    private GameObject block2;
+
+    private void Start()
     {
-        
+        ID = GetInstanceID();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (gameObject.CompareTag(collision.gameObject.tag))
+        {
+            block1 = gameObject;
+            block2 = collision.gameObject;
+            if (ID < block2.gameObject.GetComponent<MergeObject>().ID) return;
+            Vector2 mergedObjPos = new Vector2((block1.transform.position.x + block1.transform.position.x) / 2, (block1.transform.position.y + block1.transform.position.y) / 2);
+            Instantiate(Resources.Load("SquareObject"), mergedObjPos, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
     }
 }
