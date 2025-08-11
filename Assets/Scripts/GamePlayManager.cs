587b925433bd010ec4b5ba6d001fdb0eb0239e79
Thai Son <ngthaison2312@gmail.com>
2025-08-11 08:50:21 +0700

    updater pointer drag (1 bug)
    fruit not follow the pointer when player mouse down and dragging when the pointer is inactive

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -11,7 +11,12 @@ public class GamePlayManager : MonoBehaviour
 
     [SerializeField] private List<GameObject> objectList;
     [SerializeField] private GameObject currentFruit;
+
+    private BoxCollider2D dragRangeCollider;
+    public float dragRange;
+    private bool pointerIsDragging = false;
     private Vector3 offSet;
+
 
 
     private void Awake()
@@ -22,16 +27,29 @@ public class GamePlayManager : MonoBehaviour
         }
 
         pointer.transform.position = new Vector3(0f, 4f, 0f);
+        dragRangeCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     private void Start()
     {
         SpawnObject();
+        dragRange = dragRangeCollider.size.x;
     }
 
     private void Update()
     {
-        pointer.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + offSet.x, pointer.transform.position.y, pointer.transform.position.z);
+        if (pointerIsDragging)
+        {
+            pointer.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + offSet.x, pointer.transform.position.y, pointer.transform.position.z);
+            if(pointer.transform.position.x >= dragRange / 2 - currentFruit.GetComponent<CircleCollider2D>().radius / 2 - 0.5f)
+            {
+                pointer.transform.position = new Vector3(dragRange / 2 - currentFruit.GetComponent<CircleCollider2D>().radius / 2 - 0.5f, pointer.transform.position.y, pointer.transform.position.z);
+            }
+            if (pointer.transform.position.x <= -dragRange / 2 + currentFruit.GetComponent<CircleCollider2D>().radius / 2 + 0.5f)
+            {
+                pointer.transform.position = new Vector3(-dragRange / 2 + currentFruit.GetComponent<CircleCollider2D>().radius / 2 + 0.5f, pointer.transform.position.y, pointer.transform.position.z);
+            }
+        }
         if(currentFruit != null)
         {
             if(currentFruit.GetComponent<DragDrop>().isDragging == true)
@@ -46,6 +64,29 @@ public class GamePlayManager : MonoBehaviour
         StartCoroutine("RandomObject");
     }
 
+
+
+    private void OnMouseDown()
+    {
+        pointerIsDragging = true;
+        pointer.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, pointer.transform.position.y, pointer.transform.position.z);
+        offSet = pointer.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (currentFruit.GetComponent<DragDrop>().isDraggable)
+        {
+            currentFruit.GetComponent<DragDrop>().isDragging = true;
+        }
+    }
+
+
+    private void OnMouseUp()
+    {
+        pointerIsDragging = false;
+        currentFruit.GetComponent<DragDrop>().isDragging = false;
+        currentFruit.GetComponent<DragDrop>().isDraggable = false;
+        currentFruit.GetComponent<DragDrop>().rb.bodyType = RigidbodyType2D.Dynamic;
+        StartCoroutine("OnOffPointer");
+        SpawnObject();
+    }
     IEnumerator RandomObject()
     {
         int randomID = Random.Range(0, objectList.Count);
@@ -53,20 +94,11 @@ public class GamePlayManager : MonoBehaviour
         currentFruit = Instantiate(objectList[randomID], pointer.transform.position, Quaternion.identity);
     }
 
-    private void OnMouseDown()
+    IEnumerator OnOffPointer()
     {
-        if (currentFruit.GetComponent<DragDrop>().isDraggable)
-        {
-            offSet = pointer.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentFruit.GetComponent<DragDrop>().isDragging = true;
-        }
+        pointer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.75f);
+        pointer.gameObject.SetActive(true);
     }
 
-    private void OnMouseUp()
-    {
-        currentFruit.GetComponent<DragDrop>().isDragging = false;
-        currentFruit.GetComponent<DragDrop>().isDraggable = false;
-        currentFruit.GetComponent<DragDrop>().rb.bodyType = RigidbodyType2D.Dynamic;
-        SpawnObject();
-    }
 }
